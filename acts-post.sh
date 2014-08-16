79a30408ca97eea3d52e580ead30f0e39d3a6882
Alex Jurkiewicz <alex@jurkiewi.cz>
2014-08-16 12:00:15 +1000

    simpify example pre & post backup scripts

--- acts-post.sh
+++ acts-post.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# This is a post-backup script for acts. You probably want to customise it.
+# This is an example post-backup script for acts.
 
 /usr/local/bin/tarsnap --print-stats > /var/lib/munin/tarsnap-stats.sh
--- acts-pre.sh
+++ acts-pre.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# This is an example pre-backup script for acts. You probably want to customise it.
+# This is an example pre-backup script for acts.
 
-nice mysqldump --all-databases --skip-extended-insert > /root/mysqldump.today.sql
-dpkg --get-selections > /root/dpkg-selections.txt
+nice mysqldump --all-databases > /root/mysqldump.today.sql
